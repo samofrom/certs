c59f912b0fc02f9c808653441f3c2d2bb38304ac
samofrom <samofrom26@gmail.com>
2024-10-31 11:42:54 +0300

    Add router basename

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,27 +6,30 @@ import CertificateList from './services/CertificateList';
 import CheckoutForm from './services/CheckoutForm';
 import Payment from './services/Payment';
 
-export const router = createBrowserRouter([
-  {
-    element: <Page />,
-    path: '/',
-    children: [
-      {
-        index: true,
-        element: 'Not found',
-      },
-      {
-        path: '/:apiKey',
-        element: <CertificateList />,
-      },
-      {
-        path: '/:apiKey/cart',
-        element: <CheckoutForm />,
-      },
-      {
-        path: '/:apiKey/payment',
-        element: <Payment />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <Page />,
+      path: '/',
+      children: [
+        {
+          index: true,
+          element: 'Not found',
+        },
+        {
+          path: '/:apiKey',
+          element: <CertificateList />,
+        },
+        {
+          path: '/:apiKey/cart',
+          element: <CheckoutForm />,
+        },
+        {
+          path: '/:apiKey/payment',
+          element: <Payment />,
+        },
+      ],
+    },
+  ],
+  { basename: '/certs' }
+);
